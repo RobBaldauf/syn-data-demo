77af441869b5e0a18b347e5757ac9f6d99597a80
Ubuntu <robbal@vmgpu-1.sg2y4jqtumbufecwonpmhbckge.ax.internal.cloudapp.net>
2025-06-16 22:51:29 +0000

    formatted code

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,5 +1,5 @@
-
 """Image utility functions."""
+
 import cv2
 import numpy as np
 
@@ -8,46 +8,60 @@ def rgb2lab(rgb: np.ndarray) -> np.ndarray:
     """Convert an RGB image to LAB color space."""
     return cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
 
+
 def lab2rgb(lab: np.ndarray) -> np.ndarray:
     """Convert an LAB image to RGB color space."""
     return cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
+
 
 def rgb2yuv(rgb: np.ndarray) -> np.ndarray:
     """Convert an RGB image to YUV color space."""
     return cv2.cvtColor(rgb, cv2.COLOR_RGB2YUV)
 
+
 def yuv2rgb(yuv: np.ndarray) -> np.ndarray:
     """Convert a YUV image to RGB color space."""
     return cv2.cvtColor(yuv, cv2.COLOR_YUV2RGB)
 
-def srgb2lin(srgb:np.ndarray)->np.ndarray:
+
+def srgb2lin(srgb: np.ndarray) -> np.ndarray:
     """Convert an sRGB image to linear RGB color space."""
     srgb = srgb.astype(float) / 255.0
     return np.where(
-        srgb <= 0.0404482362771082, srgb / 12.92, np.power(((srgb + 0.055) / 1.055), 2.4)
+        srgb <= 0.0404482362771082,
+        srgb / 12.92,
+        np.power(((srgb + 0.055) / 1.055), 2.4),
     )
 
-def lin2srgb(lin:np.ndarray)->np.ndarray:
+
+def lin2srgb(lin: np.ndarray) -> np.ndarray:
     """Convert a linear RGB image to sRGB color space."""
     return 255 * np.where(
         lin > 0.0031308, 1.055 * (np.power(lin, (1.0 / 2.4))) - 0.055, 12.92 * lin
     )
 
+
 def get_luminance(
     linear_image: np.ndarray, luminance_conversion=[0.2126, 0.7152, 0.0722]
-)->np.ndarray:
+) -> np.ndarray:
     """Calculate the luminance of a linear RGB image.
-    
+
     Args:
         linear_image: Linear RGB image.
         luminance_conversion: Coefficients for converting RGB to luminance.
 
     Returns:
-        np.ndarray: Luminance channel of the image. 
+        np.ndarray: Luminance channel of the image.
     """
     return np.sum([[luminance_conversion]] * linear_image, axis=2)
 
-def take_luminance_from_first_chroma_from_second(luminance:np.ndarray, chroma:np.ndarray, mode:str="lab", scaling_factor:int=1)->np.ndarray:
+
+def take_luminance_from_first_chroma_from_second(
+    luminance: np.ndarray,
+    chroma: np.ndarray,
+    mode: str = "lab",
+    scaling_factor: int = 1,
+) -> np.ndarray:
     """Replace the luminance channel of the first image with the luminance channel of the second image.
     Args:
         luminance: Luminance image.
@@ -69,11 +83,11 @@ def take_luminance_from_first_chroma_from_second(luminance:np.ndarray, chroma:np
         return lin2srgb(
             np.clip(
                 lchroma
-                * ((get_luminance(lluminance) / (get_luminance(lchroma))) ** scaling_factor)[
-                    :, :, np.newaxis
-                ],
+                * (
+                    (get_luminance(lluminance) / (get_luminance(lchroma)))
+                    ** scaling_factor
+                )[:, :, np.newaxis],
                 0,
                 1,
             )
         )
- 
--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 """Synthetic data generation demo script for ControlNet."""
+
 from pathlib import Path
 from cldm.hack import disable_verbosity, enable_sliced_attention
 import hydra
@@ -8,24 +9,25 @@ from pathlib import Path
 from use_cases.mri_image_generation import MriImageGeneration
 
 
-@hydra.main(config_path="../", config_name="config",version_base=None)
-def main(cfg: DictConfig)-> None:
+@hydra.main(config_path="../", config_name="config", version_base=None)
+def main(cfg: DictConfig) -> None:
     """Main function to run for the controlnet synthetic data generation script.
-    
+
     Args:
         cfg: Global hydra config object containing all parameters.
     """
     disable_verbosity()
-    if cfg.enable_save_memory:  
+    if cfg.enable_save_memory:
         enable_sliced_attention()
     if cfg.use_case == "mri_image_gen":
         generator = MriImageGeneration(
-            save_memory=cfg.enable_save_memory, 
-            model_path= Path(cfg.canny.model_path),
+            save_memory=cfg.enable_save_memory,
+            model_path=Path(cfg.canny.model_path),
             checkpoint_path=Path(cfg.canny.checkpoint_path),
-            device="cuda" if cfg.enable_cuda else "cpu"
+            device="cuda" if cfg.enable_cuda else "cpu",
         )
-    generator.gradio_ui().launch(server_name='0.0.0.0')
+    generator.gradio_ui().launch(server_name="0.0.0.0")
+
 
 if __name__ == "__main__":
     main()
